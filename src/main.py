ba81dd7c95847c418e748db7ca9d4f97d82a0c38
hyojin425 <gywls5184@gmail.com>
2026-08-22 21:53:03 +0900

    fix: 포트 변경

--- src/main.py
+++ src/main.py
@@ -24,4 +24,4 @@ app.include_router(api_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("src.main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8001)
